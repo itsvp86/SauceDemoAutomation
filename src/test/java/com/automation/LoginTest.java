1765d34be9a6c3c58c184693b087005b65c2d472
vyankatesh panchal <vyankateshp86@gmail.com>
2025-12-18 13:44:22 +0530

    Update LoginTest.java

--- src/test/java/com/automation/LoginTest.java
+++ src/test/java/com/automation/LoginTest.java
@@ -21,8 +21,14 @@ public class LoginTest extends baseTest {
 	        Thread.sleep(4000);
 	        System.out.println("Login successful, pageTitle: " + driver.getTitle());
 	        
-	        
 
+	     // Validate Products page title
+	        String productTitle = driver.findElement(By.className("title")).getText();
+	        Assert.assertEquals(productTitle, "Products", "Login failed — Products page title mismatch");
+     	// validate login validation
+	       //String errorMsg = driver.findElement(By.cssSelector("h3[data-test='error']")).getText();
+	        //Assert.assertEquals(errorMsg, "Epic sadface: Username and password do not match any user in this service", "Error message mismatch");
+	  
 	      String expurl = "https://www.saucedemo.com/inventory.html";
 	      String acturl = driver.getCurrentUrl();
 	      Assert.assertEquals(acturl, expurl, "Login failed — user not redirecte to Product page");
